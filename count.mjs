c97e1668beda184e7e44da25a9ecc28987826561
seandunaway <git@seandunaway.com>
2023-09-21 02:39:28 -0700

    tidy

--- count.mjs
+++ count.mjs
@@ -14,23 +14,23 @@ let {values: args} = parseArgs({
         file: {short: 'f', type: 'string', default: `${homedir()}/.count`}
     },
 })
+let number = parseInt(/** @type {string} */ (args.number))
+let filename = /** @type {import('node:fs').PathLike} */ (args.file)
 
 let data
 try {
-    let file = await readFile(/** @type {import('node:fs').PathLike} */ (args.file), {encoding: 'utf8'})
+    let file = await readFile(filename, {encoding: 'utf8'})
     data = JSON.parse(file)
 } catch (error) {
     data = {}
 }
 if (typeof data[`${args.label}`] === 'undefined') data[`${args.label}`] = 0
 
-let number_integer = parseInt(/** @type {string} */ (args.number))
-if (args.increment) data[`${args.label}`] += number_integer
-if (args.decrement) data[`${args.label}`] -= number_integer
+if (args.increment) data[`${args.label}`] += number
+if (args.decrement) data[`${args.label}`] -= number
 if (args.zero) delete data[`${args.label}`]
 
 console.info(data[`${args.label}`])
 
-let json = JSON.stringify(data, undefined, 4)
-await writeFile(/** @type {import('node:fs').PathLike} */ (args.file), json)
-
+let json = JSON.stringify(data, undefined, 2)
+await writeFile(filename, json)
